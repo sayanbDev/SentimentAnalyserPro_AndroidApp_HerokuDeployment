ec3418de260d71c50fda6e35cae05a8e8b742b1c
sayanbDev <66511875+sayanbDev@users.noreply.github.com>
2020-06-07 18:23:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def get_text_prediction():
     no_of_tweets=json['no']
     no_of_tweets=int(no_of_tweets)
     if no_of_tweets<0 or no_of_tweets>200:
-        return jsonify({"test":0})
+        return jsonify({"test":1}) #limit error
     else:
         consumer_key = 'GujYFuaLVvmLYQH7tBbFM43ZX'
         consumer_secret = 'YCimJhchoxTDJwjHcATZADsGrSStP0kMsVOlSUsUMm45ERolTz'
@@ -64,74 +64,85 @@ def get_text_prediction():
         api = tweepy.API(auth)
         search=topic+' -filter:retweets'
         tweets = api.search(search, result_type="mixed", lang="en",tweet_mode="extended",count=no_of_tweets)
-        data=[clean_tweet(tweet.full_text) for tweet in tweets]
-        pos=[]
-        neg=[]
-        neu=[]
-        pos_pol=0
-        neg_pol=0
-        neu_pol=0
-        pos_urls=[]
-        neg_urls=[]
-        neu_urls=[]
-        i=0
-        
-        for tweet in data:
-            analysis = TextBlob(tweet)
-            if analysis.sentiment.polarity > 0: 
-                pos_pol=pos_pol+analysis.sentiment.polarity
-                pos.append(tweets[i])
-                pos_urls.append(f"https://twitter.com/user/status/{tweets[i].id}")
-            elif analysis.sentiment.polarity == 0: 
-                neu_pol=neu_pol+analysis.sentiment.polarity
-                neu.append(tweets[i])
-                neg_urls.append(f"https://twitter.com/user/status/{tweets[i].id}")
-            else: 
-                neg_pol=neg_pol+analysis.sentiment.polarity
-                neg.append(tweets[i])
-                neu_urls.append(f"https://twitter.com/user/status/{tweets[i].id}")
-            i+=1
-        overall=''
-        if pos_pol>neg_pol:
-            if pos_pol>neu_pol:
-                overall='positive'
+        if (len(tweets)==count:
+            data=[clean_tweet(tweet.full_text) for tweet in tweets]
+            pos=[]
+            neg=[]
+            neu=[]
+            pos_pol=0
+            neg_pol=0
+            neu_pol=0
+            pos_urls=[]
+            neg_urls=[]
+            neu_urls=[]
+            i=0
+
+            for tweet in data:
+                analysis = TextBlob(tweet)
+                if analysis.sentiment.polarity > 0: 
+                    pos_pol=pos_pol+analysis.sentiment.polarity
+                    pos.append(tweets[i])
+                    pos_urls.append(f"https://twitter.com/user/status/{tweets[i].id}")
+                elif analysis.sentiment.polarity == 0: 
+                    neu_pol=neu_pol+analysis.sentiment.polarity
+                    neu.append(tweets[i])
+                    neu_urls.append(f"https://twitter.com/user/status/{tweets[i].id}")
+                else: 
+                    neg_pol=neg_pol+analysis.sentiment.polarity
+                    neg.append(tweets[i])
+                    neu_urls.append(f"https://twitter.com/user/status/{tweets[i].id}")
+                i+=1
+            overall=''
+            pos_per=int((len(pos)/no_of_tweets)*100)
+            neg_per=int((len(neg)/no_of_tweets)*100)
+            neu_per=int((len(neu)/no_of_tweets)*100)
+            if (pos_per+neg_per+neu_per)!=100:
+                if pos_per==neu_per:
+                    overall='TIE between POS & NEU'
+                    pos_per+=0.5
+                    neu_per+=0.5
+                elif neg_per==neu_per:
+                    overall='TIE between NEG & NEU'
+                    neg_per+=0.5
+                    neu_per+=0.5
+                else:
+                    if pos_pol> (0-neg_pol):
+                        overall='POSITIVE'
+                    else:
+                        overall='NEGATIVE'
+
+
             else:
-                overall='neutral'
-        else:
-            if neg_pol>neu_pol:
-                overall='negative'
-            else:
-                overall='neutral'
-        pos_per=int((len(pos)/no_of_tweets)*100)
-        neg_per=int((len(neg)/no_of_tweets)*100)
-        neu_per=int((len(neu)/no_of_tweets)*100)
-        if (pos_per+neg_per+neu_per)!=100:
-            if overall=='positive':
-                pos_per=pos_per+1
-            elif overall=='negative':
-                neg_per+=1
-            else:
-                neu_per+=1
+                c=max([pos_per,neg_per,neu_per])
+                if c==pos_per:
+                    overall='POSITIVE'
+                elif c==neg_per:
+                    overall='NEGATIVE'
+                else:
+                    overall='NEUTRAL'
             
-        return jsonify({"test":1,
-                        "no_of_tweets":no_of_tweets,
-                        "overall":overall,
-                        "search":topic,
-                        "pos":len(pos),
-                        "neg":len(neg),
-                        "neu":len(neu),
-                        "pos%":pos_per,
-                        "neg%":neg_per,
-                        "neu%":neu_per,
-                        "urls":{"pos":pos_urls,
-                                  "neg":neg_urls,
-                                  "neu":neu_urls
-                                  },
-                        "tweets":{"pos":[tweet.full_text for tweet in pos],
-                                  "neg":[tweet.full_text for tweet in neg],
-                                  "neu":[tweet.full_text for tweet in neu]
-                                  }})
-        
+
+            return jsonify({"test":0,
+                            "no_of_tweets":no_of_tweets,
+                            "overall":overall,
+                            "search":topic,
+                            "pos":len(pos),
+                            "neg":len(neg),
+                            "neu":len(neu),
+                            "pos%":pos_per,
+                            "neg%":neg_per,
+                            "neu%":neu_per,
+                            "urls":{"pos":pos_urls,
+                                    "neg":neg_urls,
+                                    "neu":neu_urls
+                                    },
+                            "tweets":{"pos":[tweet.full_text for tweet in pos],
+                                      "neg":[tweet.full_text for tweet in neg],
+                                      "neu":[tweet.full_text for tweet in neu]
+                                      }})
+        else:
+            return jsonify({"test":2}) #topic error
+            
     
     
 
